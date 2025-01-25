b33b7c852f1b9f5fe15a0ee3a0a77cada7937f27
Louis-Douglas <contact@LouisDouglas.com>
2025-01-25 12:27:50 +0000

    Setup utils.py to contain functions for use by system a & b

---
+++ Histogram/utils.py
@@ -0,0 +1,10 @@
+import cv2
+
+# Calculate the histogram for each colour channel, return as a list
+def calculate_histogram(image):
+    histograms = []
+    colours = ("b", "g", "r")
+    for channel, colour in enumerate(colours):
+        hist = cv2.calcHist([image], [channel], None, [256], [0, 256])
+        histograms.append(hist)
+    return histograms
