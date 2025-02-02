a853343db223609c617a4d9cb3760c5248229604
Louis-Douglas <contact@LouisDouglas.com>
2025-02-02 20:30:42 +0000

    Add normalise_image function to utils

--- Histogram/utils.py
+++ Histogram/utils.py
@@ -3,6 +3,8 @@ import numpy as np
 from torchvision.ops import box_iou, distance_box_iou
 import matplotlib.pyplot as plt
 
+# Modified from: https://docs.opencv.org/3.4/d8/dbc/tutorial_histogram_calculation.html &
+# https://pyimagesearch.com/2014/07/14/3-ways-compare-histograms-using-opencv-python/
 def calculate_histogram(image):
     """
     Computes the colour histograms for an image across the Blue, Green, and Red channels.
@@ -20,6 +22,7 @@ def calculate_histogram(image):
         histograms.append(hist)
     return histograms
 
+# Modified from: https://docs.opencv.org/3.4/d8/dc8/tutorial_histogram_comparison.html
 def compare_histograms(hist1, hist2):
     """
     Compares two color histograms using the Bhattacharyya distance.
@@ -114,3 +117,26 @@ def visualise_histogram(histograms, title="Histogram"):
     plt.legend(loc='upper right')
     plt.grid(alpha=0.3)
     plt.show()
+
+# Modified from: https://www.geeksforgeeks.org/normalize-an-image-in-opencv-python/
+def normalise_image(image):
+    """
+    Normalises the brightness of an image by converting it to the HSV colour space,
+    applying min-max normalisation to the brightness channel, and converting it back to BGR.
+
+    Args:
+        image (numpy.ndarray): The input image in BGR format (as loaded by OpenCV).
+
+    Returns:
+        numpy.ndarray: The normalised image in BGR format.
+    """
+    # Convert the image to HSV colour space to obtain the brightness channel
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Normalise the brightness channel
+    hsv_image[:, :, 2] = cv2.normalize(hsv_image[:, :, 2], None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
+    # Convert back to BGR colour space
+    normalised_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR)
+
+    return normalised_image
