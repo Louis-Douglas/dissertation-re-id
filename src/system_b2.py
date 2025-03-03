880f9a5172d9359a334837728ee2182f91b1dc3c
Louis-Douglas <contact@LouisDouglas.com>
2025-03-03 15:30:38 +0000

    Implement system_b2 that uses standardised evaluation metrics (query/gallery)

---
+++ src/system_b2.py
@@ -0,0 +1,42 @@
+import glob
+import numpy as np
+
+from src.utils.validation_utils import evaluate_rank_map, visualize_reid_results
+from src.utils.segmentation_utils import get_processed_images
+
+def main():
+    query_image_paths = sorted(glob.glob("../datasets/Torch-Dataset/query/*/*.png"))
+    gallery_image_paths = sorted(glob.glob("../datasets/Torch-Dataset/gallery/*/*.png"))
+
+    moda_model_path = "../Training/modanet-seg2.pt"  # Use a YOLO model trained for clothing segmentation
+    coco_model_path = "../Training/yolo11x-seg.pt"  # Use a YOLO model trained for person segmentation
+
+    query_image_files = get_processed_images(query_image_paths, coco_model_path, moda_model_path)
+    gallery_image_files = get_processed_images(gallery_image_paths, coco_model_path, moda_model_path)
+
+    num_query = len(query_image_paths)
+    num_gallery = len(gallery_image_paths)
+
+    # Create distance matrix (query × gallery)
+    dist_matrix = np.zeros((num_query, num_gallery), dtype=np.float32)
+
+    # Fill in similarity values
+    for i, query_img in enumerate(query_image_files):
+        for j, gallery_img in enumerate(gallery_image_files):
+            dist = query_img.compare_processed_image(gallery_img)  # Your similarity function
+            dist_matrix[i, j] = dist  # Assign the similarity score
+
+    # Convert to numpy
+    similarity_matrix = 1 / (1 + dist_matrix)  # Invert to make higher values better
+
+
+    print(similarity_matrix)
+
+    # Evaluate Re-ID Performance
+    evaluate_rank_map(similarity_matrix, query_image_paths, gallery_image_paths)
+
+    # Visualise the results
+    visualize_reid_results(query_image_paths, gallery_image_paths, similarity_matrix, top_k=5)
+
+if __name__ == "__main__":
+    main()
