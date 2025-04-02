68ca32699a0537a1a5e066ee965b1eb6a437cc8e
Louis-Douglas <contact@LouisDouglas.com>
2025-04-02 14:18:56 +0100

    Remove unused values

--- src/core/processed_segment.py
+++ src/core/processed_segment.py
@@ -9,15 +9,10 @@ class ProcessedSegment:
         yolo_mask (torch.Tensor or numpy.ndarray): Segmentation mask.
         # yolo_confidence (float): Detection confidence score.
         # yolo_bbox (list): Bounding box [x1, y1, x2, y2].
-        hist (numpy.ndarray or None): Colour histogram.
-        colour_description (str): Description of object colours.
     """
     def __init__(self, image, class_id, class_name, box, mask):
         self.image = image
         self.class_id = class_id
         self.class_name = class_name
         self.yolo_mask = mask
-        # self.yolo_confidence = float(box.conf[0].item())  # Confidence score
         self.yolo_box = box
-        self.hist = None
-        self.colour_description = ""
